39193a0dc75fd975d131dbaa9ca6135310bcedab
Berat Hündürel <berat.hundurel@bilgiedu.net>
2025-02-13 14:13:03 +0300

    implementing response

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::{net::TcpListener, str};
+
+struct Response {
+    message_size: i32,
+    header: Header,
+
+}
+
+struct Header{
+    correlation_id: i32,
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
@@ -7,7 +17,12 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(_stream) => {
-                println!("accepted new connection");
+                Response {
+                    message_size: 0,
+                    header: Header {
+                        correlation_id: 7,
+                    }
+                };
             }
             Err(e) => {
                 println!("error: {}", e);
