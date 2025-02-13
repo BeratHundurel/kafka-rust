ed91f3962afb044a70c8ae194c177133d88660b0
Berat Hündürel <berat.hundurel@bilgiedu.net>
2025-02-13 14:21:34 +0300

    send message with buffer

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,30 @@
 #![allow(unused_imports)]
-use std::{net::TcpListener, str};
+use std::{io::Write, net::TcpListener, str};
 
 struct Response {
     message_size: i32,
     header: Header,
-
 }
 
-struct Header{
+struct Header {
     correlation_id: i32,
 }
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
-    
+
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                Response {
-                    message_size: 0,
-                    header: Header {
-                        correlation_id: 7,
+            Ok(mut _stream) => {
+                let buffer = [0, 0, 0, 0, 0, 0, 0, 7];
+                match _stream.write(&buffer) {
+                    Ok(_) => {
+                        println!("message sent");
                     }
-                };
+                    Err(e) => {
+                        println!("error: {}", e);
+                    }
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
